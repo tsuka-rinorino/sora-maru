7428cd40a2b7f2d299e8da66f988034cc2f2fab3
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-05-06 22:51:46 +0900

    window幅を固定

--- src/components/wrap/index.vue
+++ src/components/wrap/index.vue
@@ -11,11 +11,8 @@
 
 <style lang="scss">
 .wrap {
-  max-width: 1152px;
+  width: 1152px;
   padding-left: 312px;
   padding-right: 32px;
-  @media (min-width: 1056px) {
-    padding-left: 344px;
-  }
 }
 </style>
